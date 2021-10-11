a959ed4b0640e3e8f7aebd552ec7f89895adfe03
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-10-11 14:08:13 +0800

    Add eslint plugin for automated syntax checks

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
+import PropTypes from 'prop-types';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCDF3MKVPdtESttr1oKsult-FDnv5IV2_0",
-  authDomain: "schoolwork-1cc3c.firebaseapp.com",
-  projectId: "schoolwork-1cc3c",
-  storageBucket: "schoolwork-1cc3c.appspot.com",
-  messagingSenderId: "957168917113",
-  appId: "1:957168917113:web:49d5936bb097e69ea692a8",
-  measurementId: "G-Z4D5YGZEZT"
+  apiKey: "AIzaSyDk7VMMBwr6bqDsrzbUYGpnthHaMioYe2s",
+  authDomain: "chat-app-15946.firebaseapp.com",
+  projectId: "chat-app-15946",
+  storageBucket: "chat-app-15946.appspot.com",
+  messagingSenderId: "533178884935",
+  appId: "1:533178884935:web:ef9483d776d0107ffe6262",
+  measurementId: "G-P58VBCQ0SY"
 })
 
 const auth = firebase.auth();
@@ -111,7 +112,6 @@ function ChatRoom() {
   </>)
 }
 
-
 function ChatMessage(props) {
   const { text, uid, photoURL } = props.message;
 
@@ -125,5 +125,8 @@ function ChatMessage(props) {
   </>)
 }
 
+ChatMessage.propTypes = {
+  message: PropTypes.any,
+};
 
 export default App;
